1141f7247635ca95c1b936851d29054fe10f97c7
PierreSwtich <skamper28@gmail.com>
2023-01-10 15:51:49 +0100

    playwright

---
+++ playwright.config.js
@@ -0,0 +1,29 @@
+const { devices } = require('@playwright/test');
+
+const config = {
+  testDir: './tests',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000
+  },
+  reporter: [
+    ['html'],
+    ['line'], 
+    ['allure-playwright']
+  ],
+  use: {
+    actionTimeout: 0,
+    trace: 'on-first-retry',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+  ],
+};
+
+module.exports = config;
